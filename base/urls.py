f25bc992354a57fef42c7169a04a50bdad072719
Ham乙a Wa丂eem <120158214+Humza-Waseem@users.noreply.github.com>
2024-01-27 23:54:52 +0500

    Restriction + User Login +page restrictions

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,10 @@ from . import views  # we have imported views from base/views.py  that is in the
 
 ############  here we have made a list of url paterns    ###############
 urlpatterns  = [
+    path('login/',views.UserLogin,name = "UserLogin"),
+    path('logout/'   ,views.UserLogout,name = "UserLogout"),
     path('', views.home, name='home'), # the path() function takes 3 arguments: 1st is the url, 2nd is the view function, 3rd is the name of the url  #  the name is optional, but it is good to use it
-
+    path('register/'   ,views.registerUser,name = "register"),
     path('room/<str:pk>/',views.room, name ="room"),   # 'pk' stands for Primary Key  and it is set as string.......
     path('create-room/',views.CreateRoom, name = "create-room"),  # url of the create room page
     path('update-room/<str:pk>',views.UpdateRoom, name = "update-room") , # url of the update room page
